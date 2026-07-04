35272ef1af7108cde526da14fbd70931758012b0
ukuq <ukuq@qq.com>
2026-07-04 17:30:03 +0800

    Install wasm target for Ruffle web build

--- .github/scripts/build-ruffle-selfhosted.sh
+++ .github/scripts/build-ruffle-selfhosted.sh
@@ -5,6 +5,7 @@ script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 repo_dir="$(cd -- "$script_dir/../.." && pwd)"
 
 cd "$repo_dir/../ruffle/web"
+rustup target add wasm32-unknown-unknown
 npm ci
 npm run build --workspace=ruffle-core
 npm run build --workspace=ruffle-selfhosted
